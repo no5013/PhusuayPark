a552f6ea4acf30c63d6f78bd19a69c3414e5f170
Pongsakorn Somsri <pongsakorns@gmail.com>
2016-11-18 02:57:53 +0700

    Contact can send email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,10 +50,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: ENV["GMAIL_DOMAIN"],
+  domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  user_name: "pongsakorns@gmail.com",
+  password: "50135013"
   }
 end
