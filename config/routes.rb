5f5a2171b77a195d178f1fa7acbe6c03dd12b810
Pongsakorn Somsri <pongsakorns@gmail.com>
2017-02-11 13:15:17 +0700

    open closed route

--- config/routes.rb
+++ config/routes.rb
@@ -27,8 +27,8 @@ Rails.application.routes.draw do
   match '/book',     to: 'books#new',             via: 'get'
   resources "books", only: [:new, :create]
 
-  # match '/promotions',     to: 'promotions#index',             via: 'get'
-  # resources "promotions"
+  match '/promotions',     to: 'promotions#index',             via: 'get'
+  resources "promotions"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
