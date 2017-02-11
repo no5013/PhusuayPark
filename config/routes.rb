29203e16e9cbafc8edba725b33303fe91f56b431
Pongsakorn Somsri <pongsakorns@gmail.com>
2017-02-11 23:49:43 +0700

    Make single user system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations"}
+
   get 'page_controller/home'
 
   get 'page_controller/rooms'
